63e3a73ec8ba8361bfb4e71d12fed9b85de17e1c
rodrigoayala31 <ayalarodrigo06@gmail.com>
2025-08-13 23:45:29 -0600

    human choice function

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -20,38 +20,3 @@ function getHumanChoice() {
 
     return choice;
 }
-
-function playRound(humanChoice, computerChoice) {
-
-    const humanChoiceLowerCase = humanChoice.toLowerCase();
-    
-    if (humanChoiceLowerCase === "rock" && computerChoice === "scissors") {
-        humanScore += 1;
-        console.log("You win! Rocks beats Scissors");
-    } else if (humanChoiceLowerCase === "paper" && computerChoice === "rock") {
-        humanScore += 1;
-        console.log("You win! Paper beats Rock");
-    } else if (humanChoiceLowerCase === "scissors" && computerChoice === "paper") {
-        humanScore += 1;
-        console.log("You win! Scissors beats Paper")
-    } else if (humanChoiceLowerCase === "rock" && computerChoice === "paper") {
-        computerScore += 1;
-        console.log("You lose! Paper beats Rocks");
-    } else if (humanChoiceLowerCase === "paper" && computerChoice === "scissors"){
-        computerScore += 1;
-        console.log("You lose! Scissors beats Paper");
-    } else if (humanChoiceLowerCase === "scissors" && computerChoice === "rock") {
-        computerScore += 1;
-        console.log("You lose! Rock beats Paper");
-    } else {
-        console.log("It's a tie!");
-    }
-}
-
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-console.log("Human: ", humanSelection);
-console.log("Computer: ", computerSelection);
-
-playRound(humanSelection, computerSelection);
